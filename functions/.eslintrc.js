c70eef7b21a0039a400ec28936576e6804d71c7b
Sam Jeeves <jeevcat@users.noreply.github.com>
2022-02-24 12:48:58 +0100

    Fix eslint error

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -25,6 +25,6 @@ module.exports = {
   rules: {
     quotes: ["error", "double"],
     "import/no-unresolved": 0,
-    "no-non-null-assertion": false,
+    "@typescript-eslint/no-non-null-assertion": 0,
   },
 };
