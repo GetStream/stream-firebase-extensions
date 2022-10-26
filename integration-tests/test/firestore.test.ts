2f0179470b31ccfd2598a8df6158140c4018919d
Sam Jeeves <jeevcat@users.noreply.github.com>
2022-10-26 10:00:07 +0200

    Test cleanup

--- integration-tests/test/firestore.test.ts
+++ integration-tests/test/firestore.test.ts
@@ -4,6 +4,9 @@ import { getFirestore } from "firebase-admin/firestore";
 import * as stream from "getstream";
 import { FeedAPIResponse } from "getstream";
 
+// Default jest timeout is 5 secs
+jest.setTimeout(30000);
+
 console.log("Loading env vars...");
 for (const path of [
   "extensions/firestore-activity-feeds.env.local",
@@ -29,20 +32,20 @@ const verb = "run";
 const object = "exercise:42";
 
 describe("create firestore document", () => {
-  beforeAll(async () => {
+  afterEach(async () => {
     // Clear all activities
     const feeds = stream.connect(api_key, api_secret);
     const user1 = feeds.feed(feedType, feedId);
     const allActivities = await user1.get();
     await Promise.all(allActivities.results.map((r: FeedAPIResponse["results"][number]) => user1.removeActivity(r.id)));
+  });
 
+  test("verify activity creation", async () => {
     await firestore.doc(`${collectionId}/${feedType}/${feedId}/${foreignId}`).create({ actor, verb, object });
 
     // Wait for triggers to execute
     await new Promise((resolve) => setTimeout(resolve, 2000));
-  });
 
-  test("verify activity creation", async () => {
     const feeds = stream.connect(api_key, api_secret);
     const user1 = feeds.feed(feedType, feedId);
     const response = await user1.get();
