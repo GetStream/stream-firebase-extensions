86894cb951b965ddb5cf3628a71d3c3d9e6945b2
Stefan <stefan.blos@gmail.com>
2025-02-20 09:27:53 +0100

    Auth tests are working

--- integration-tests/test/auth.test.ts
+++ integration-tests/test/auth.test.ts
@@ -1,153 +1,106 @@
-import * as dotenv from "dotenv";
-import { initializeApp as initializeFirebaseAdmin } from "firebase-admin/app";
-import { getAuth as getAuthAdmin } from "firebase-admin/auth";
-import { initializeApp as initializeFirebaseClient } from "firebase/app";
-import { connectAuthEmulator, getAuth as getAuthClient, signInWithEmailAndPassword } from "firebase/auth";
-import { connectFunctionsEmulator, getFunctions, httpsCallable } from "firebase/functions";
-import * as stream from "getstream";
-import { StreamChat, UserResponse } from "stream-chat";
-import { expectRecent } from "./util";
+import { initializeApp } from 'firebase-admin/app';
+import { initializeApp as initializeFirebaseClient } from 'firebase/app';
+import { Auth, getAuth, UserRecord } from 'firebase-admin/auth';
+import { StreamChat, UserResponse } from 'stream-chat';
+import * as dotenv from 'dotenv';
+import { connectFunctionsEmulator, Functions } from 'firebase/functions';
+import { getFunctions, httpsCallable } from 'firebase/functions';
+import { createUser, displayName, email, expectRecent, photoUrl } from './util';
 
-for (const path of ["extensions/auth-activity-feeds.env.local", "extensions/auth-activity-feeds.secret.local"]) {
+for (const path of [
+  'extensions/auth-activity-feeds.env.local',
+  'extensions/auth-activity-feeds.secret.local',
+]) {
   const { error, parsed } = dotenv.config({ path });
   if (error) {
     console.error(error);
   }
 }
 
-const api_key = process.env.STREAM_API_KEY!;
-const api_secret = process.env.STREAM_API_SECRET!;
+describe('Auth Chat Tests', () => {
+  let userId: string;
+  let auth: Auth;
+  let functions: Functions;
+  let streamClient: StreamChat;
 
-initializeFirebaseAdmin();
-const adminAuth = getAuthAdmin();
-
-const app = initializeFirebaseClient({
-  projectId: process.env.GCLOUD_PROJECT,
-  apiKey: "fake",
-});
-const functions = getFunctions(app);
-connectFunctionsEmulator(functions, "localhost", 5001);
-const clientAuth = getAuthClient(app);
-connectAuthEmulator(clientAuth, "http://127.0.0.1:9099", { disableWarnings: true });
-
-const chatServer = new StreamChat(api_key, api_secret);
-const chatClient = new StreamChat(api_key, { allowServerSideConnect: true });
-
-// Default jest timeout is 5 secs
-jest.setTimeout(30000);
-
-const email = "user@example.com";
-const password = "secretPassword";
-const name = "John Doe";
-const image = "https://api.lorem.space/image/face";
-
-let uid: string;
-describe("create user", () => {
-  // Create Firebase user to trigger extension functions
   beforeAll(async () => {
-    ({ uid } = await adminAuth.createUser({
-      email,
-      password,
-      displayName: name,
-      photoURL: image,
-      emailVerified: true,
-      phoneNumber: "+11234567890",
-      disabled: false,
-    }));
+    // Check that the API key and secret are set
+    const api_key = process.env.STREAM_API_KEY;
+    const api_secret = process.env.STREAM_API_SECRET;
+    if (!api_key || !api_secret) {
+      throw new Error('STREAM_API_KEY and STREAM_API_SECRET must be set');
+    }
 
-    // Wait for triggers to execute
+    // Initialize Firebase tools
+    initializeApp();
+    auth = getAuth();
+
+    // Initialize Stream client
+    streamClient = new StreamChat(api_key, api_secret);
+
+    const { users } = await streamClient.queryUsers({ email: email });
+    for (const user of users) {
+      await streamClient.deleteUser(user.id);
+    }
+  });
+
+  afterEach(async () => {
+    if (userId) {
+      try {
+        // Delete the user from Firebase
+        await auth.deleteUser(userId);
+      } catch (error) {
+        console.log('Error deleting Firebase user:', error);
+      }
+
+      try {
+        // Delete the user from Stream
+        await streamClient.deleteUser(userId);
+      } catch (error) {
+        console.log('Error deleting Stream user:', error);
+      }
+    }
+  });
+
+  test('Verify no user exists', async () => {
+    // Given
+
+    // When
+
+    // Then
+    const { users } = await streamClient.queryUsers({ email: email });
+    expect(users.length).toBe(0);
+  });
+
+  test('Verify Firebase user creation syncs with Stream Chat', async () => {
+    // Given
+
+    // When
+    const userRecord = await createUser(auth);
+    userId = userRecord.uid;
     await new Promise((resolve) => setTimeout(resolve, 2000));
-  });
 
-  test("verify chat user creation", async () => {
-    // Verify creation of user
-    const { users } = await chatServer.queryUsers({ id: uid });
-    const user = users.find((u) => u.id === uid);
-    expect(user).not.toBeNull();
-    expect(user?.name).toBe(name);
-    expect(user?.image).toBe(image);
-  });
-
-  test("verify feeds user creation", async () => {
-    // Verify creation of user
-    const feeds = stream.connect(api_key, api_secret);
-
-    const { data: user } = await feeds.user(uid).get();
-    expect(user).not.toBeNull();
-    expect(user?.name).toBe(name);
-    expect(user?.profileImage).toBe(image);
+    // Then
+    const { users } = await streamClient.queryUsers({ id: userId });
+    const user = users.find((u) => u.id === userId);
+    expect(user).not.toBeUndefined();
     expect(user?.email).toBe(email);
-  });
-});
-
-describe("generate tokens", () => {
-  beforeAll(async () => {
-    await signInWithEmailAndPassword(clientAuth, email, password);
+    expect(user?.name).toBe(displayName);
+    expect(user?.image).toBe(photoUrl);
   });
 
-  test("create and validate chat token", async () => {
-    const getStreamUserToken = httpsCallable<undefined, string>(functions, "ext-auth-chat-getStreamUserToken");
-    const { data: token } = await getStreamUserToken();
-
-    await chatClient.connectUser({ id: uid }, token);
-    expect(chatClient.user.name).toBe(name);
-    await chatClient.disconnectUser();
-
-    type Response = { users: { [key: string]: UserResponse } };
-    const revokeStreamUserToken = httpsCallable<undefined, Response>(functions, "ext-auth-chat-revokeStreamUserToken");
-    const {
-      data: { users },
-    } = await revokeStreamUserToken();
-    expect(users).toHaveProperty(uid);
-    expect(users[uid].name).toBe(name);
-    expectRecent(new Date(users[uid].revoke_tokens_issued_before));
-
-    // Seems we need to wait here to avoid flakiness
+  test('Verify Firebase user deletion syncs with Stream Chat', async () => {
+    // Given
+    const userRecord = await createUser(auth);
     await new Promise((resolve) => setTimeout(resolve, 2000));
 
-    await expect(chatClient.connectUser({ id: uid }, token)).rejects.toThrowError("token has been revoked");
-  });
-
-  test("create and validate feeds token", async () => {
-    const getStreamUserToken = httpsCallable<undefined, string>(
-      functions,
-      "ext-auth-activity-feeds-getStreamUserToken"
-    );
-    const { data: token } = await getStreamUserToken();
-
-    const feeds = stream.connect(api_key, token);
-    const { data: user } = await feeds.currentUser.get();
-    expect(user).not.toBeNull();
-    expect(user?.name).toBe(name);
-    expect(user?.profileImage).toBe(image);
-    expect(user?.email).toBe(email);
-  });
-});
-
-describe("delete user", () => {
-  // Delete the Firebase user to trigger extension functions
-  beforeAll(async () => {
-    await adminAuth.deleteUser(uid);
-
-    // Wait for triggers to execute
+    // When
+    await auth.deleteUser(userRecord.uid);
     await new Promise((resolve) => setTimeout(resolve, 2000));
-  });
 
-  test("verify chat user deletion", async () => {
-    const { users } = await chatServer.queryUsers({ id: uid });
-    const user = users.find((u) => u.id === uid);
+    // Then
+    const { users } = await streamClient.queryUsers({ id: userRecord.uid });
+    const user = users.find((u) => u.id === userRecord.uid);
     expect(user).toBeUndefined();
   });
-
-  test("verify feeds user deletion", async () => {
-    const feeds = stream.connect(api_key, api_secret);
-    await expect(feeds.user(uid).get()).rejects.toThrowError("User does not exist");
-  });
 });
-
-// TODO test creating firestore document
-// TODO verify creating firestore document in feeds
-// TODO test updating firestore document
-// TODO verify updating firestore document in feeds
-// TODO test deleting firestore document
-// TODO verify deleting firestore document in feeds
--- integration-tests/test/util.ts
+++ integration-tests/test/util.ts
@@ -1,3 +1,25 @@
+import { Auth, UserRecord } from 'firebase-admin/auth';
+
 export function expectRecent(date: Date, thresholdMs = 10000) {
-  expect(Math.abs(new Date().getTime() - date.getTime())).toBeLessThan(thresholdMs);
+  expect(Math.abs(new Date().getTime() - date.getTime())).toBeLessThan(
+    thresholdMs
+  );
 }
+
+export async function createUser(auth: Auth): Promise<UserRecord> {
+  return await auth.createUser({
+    email: email,
+    emailVerified: true,
+    phoneNumber: phoneNumber,
+    password: password,
+    displayName: displayName,
+    photoURL: photoUrl,
+  });
+}
+
+// User credentials
+export const email = 'user@example.com';
+export const password = 'secretPassword';
+export const phoneNumber = '+11234567890';
+export const displayName = 'John Doe';
+export const photoUrl = 'https://example.com/johndoe.jpg';
